7e81205964849112e040e655a6e255fe0e41ed39
PerYngve <alexpage23@gmail.com>
2024-03-11 12:47:03 -0600

    Update Conexion.java

--- Conexion.java
+++ Conexion.java
@@ -1,18 +1,87 @@
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Conexion {
-    private static final String URL = "jdbc:mysql://localhost:3306/estudiantes";
-    private static final String USUARIO = "root";
-    private static final String CONTRASEÑA = "";
-    
-    public static Connection obtenerConexion() {
-        try {
-            return DriverManager.getConnection(URL, USUARIO, CONTRASEÑA);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+
+public class conexion {
+	
+	
+	private static String driver="com.mysql.jdbc.Driver";
+	private static String usuario="root";
+	private static String password="";
+	private static String url="jdbc:mysql://localhost:3306/alumnos";
+	
+	static {
+		try {
+			Class.forName(driver);
+			System.out.println("conexion con mysql" );
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("error en el driver" );
+		}
+		
+	}
+	
+				Connection con=null;
+		public Connection getConnection() {
+	
+			
+			
+			try {
+			
+				 con=DriverManager.getConnection(url, usuario, password);
+				 System.out.println("conectado a mysql" );
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("error de conexion");
+			
+			}
+			return con;
+			
+			
+		}
+		
+		
+		
+		public Connection close() {
+		
+
+			try {
+			
+				 con.close();
+				 System.out.println("se cerro la conexion exitosamente" );
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("error de conexion");
+			
+			}
+			return con;
+		}
+		
+		
+		
+		
+		public static void main(String[] args) {
+			
+		
+		conexion db=new conexion();
+		db.getConnection();
+		//db.close();
+	
+	
+}  
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 }
